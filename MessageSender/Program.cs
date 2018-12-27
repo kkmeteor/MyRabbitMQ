7e15616bcb4725e583fd4b3db57ff7a4f448436b
kkmeteor <kkmeteor@126.com>
2018-12-28 00:12:49 +0800

    123

---
+++ MessageSender/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            factory.UserName = "matengfei";
+            factory.Password = "matengfei";
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
+                    string message = "Hello World";
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish("", "hello", null, body);
+                    Console.WriteLine(" set {0}", message);
+                }
+            }
+        }
+    }
+}
